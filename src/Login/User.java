1b76d84b7002a10d93a4674d2ec1314297250ea4
Movlan Aliyev <movlan@live.com>
2022-08-21 00:27:34 -0700

    work on process

--- src/Login/User.java
+++ src/Login/User.java
@@ -4,6 +4,7 @@ public class User {
     public static void main(String[] args) {
         System.out.println("Field for user");
 
+        System.out.println("I'm dev working on project");
 
     }
 }
