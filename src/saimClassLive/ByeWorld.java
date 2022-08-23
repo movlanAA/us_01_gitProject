4de41c6f58095f2462cd5dbe81484d89eb50f85a
Movlan Aliyev <movlan@live.com>
2022-08-23 08:31:18 -0700

    adding bye world.java

---
+++ src/saimClassLive/ByeWorld.java
@@ -0,0 +1,8 @@
+package saimClassLive;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("5 weeks left ");
+        System.out.println("1.5 weeks of Git");
+    }
+}
